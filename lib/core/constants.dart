b1ca3d19694119b16fdd5125dcd6d9584d4bf47f
mariotmbross@hotmail.com <mtejada@controlla.com.mx>
2024-03-25 20:30:09 -0600

    feat: fonts

---
+++ lib/core/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+Color pokeRed = const Color(0xFFEF5350);
---
+++ lib/themes/poke_theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../core/constants.dart';
+
+final ThemeData pokedexTheme = ThemeData(
+  primaryColor: pokeRed,
+  appBarTheme: AppBarTheme(
+    backgroundColor: pokeRed,
+    systemOverlayStyle: SystemUiOverlayStyle.light,
+    titleTextStyle: const TextStyle(
+        color: Colors.white, fontFamily: 'Pokemon', fontSize: 10),
+  ), // Gris claro
+  bottomAppBarTheme: BottomAppBarTheme(color: pokeRed),
+  scaffoldBackgroundColor: const Color(0xFFFFFFFF),
+  fontFamily: 'Pokemon', // Fuente predeterminada
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF333333)), // Texto grande y negrita
+    displayMedium: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF333333)), // Texto grande y negrita
+    displaySmall: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF333333)), // Texto grande y negrita
+    headlineMedium: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF333333)), // Texto grande y negrita
+    headlineSmall: TextStyle(
+        fontSize: 10,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF333333)), // Texto grande y negrita
+    titleLarge: TextStyle(
+        fontSize: 8,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF333333)), // Texto grande y negrita
+    bodyLarge:
+        TextStyle(fontSize: 16, color: Color(0xFF333333)), // Texto normal
+    bodyMedium:
+        TextStyle(fontSize: 14, color: Color(0xFF333333)), // Texto normal
+  ),
+  colorScheme:
+      ColorScheme.fromSwatch().copyWith(secondary: const Color(0xFF29B6F6)),
+);
